9d7e66decfcc94b5a5cd58f09ac6790af94335c6
g3nius <131481963+codegeniuz@users.noreply.github.com>
2023-07-22 16:17:36 -0500

    Add files via upload

---
+++ donate.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+  <html lang="en" class="donation-banner-iframe">
+    <head><title>Donate</title><style>body{margin:0}</style>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+      <script src="js/jquery-1.10.2.min.js"></script>
+      <script src="js/polyfill.min.js"></script>
+      <script src="js/analytics_1.js"></script>
+          <script src="js/donation-banner.min.js" type="text/javascript"></script>
+    </head>
+    <body class="js-ia-iframe" style="margin:0">
+          <link href="css/banner.css" rel="stylesheet" type="text/css">
+    <link href="css/chevron.css" rel="stylesheet" type="text/css">
+
+    <header id="donate_banner" class="row js-donate-banner template-chevron background-default chevron-image-white  chevron-color-default formdesign-minimal" itemscope itemtype="http://schema.org/WPAdBlock" data-donationSourceData="ctx=wb;uid=7f0777823ced450f791c28991abcbe00;ref=https%3A%2F%2Fweb.archive.org%2Fweb%2F20130522234421%2Fhttp%3A%2F%2Fprimedice.com%2F" data-exp="July Deploy" data-variant="Jul2023WBminwig2" data-platform="wb" data-baseUrl="https://archive.org" data-bannerTemplate="chevron" data-bannerBackground="default" data-snowfall="off" data-selectedTextColor="#fff" data-selectedFillColor="#23765D" data-paymentOptionFillColor="#23765D" data-paymentOptionTextColor="#fff" data-formBgColor data-formTextColor data-donatePageSelectedTextColor data-donatePageSelectedFillColor data-variantDollarAmounts="[5, 27, 50, 100]" data-debugMode="false" style>
+      <button id="donate-close-button" class="donate-close" type="button" aria-label="Close banner">
+        <span class="sr-only">Close banner</span>
+        <img src="fonts/close-circle-dark.svg" id="banner-close-image-dark">
+        <img src="fonts/close-circle-white.svg" id="banner-close-image-white">
+      </button>
+      <div id="banner-body-container" style>
+        <div id="banner-body-left-container" class="show-separator" style>
+                    <div class="donate-body">
+                                      <div id="chevron-image-container" style></div>
+            
+            <div class="main-content">
+              <div class="appeal-text" style>
+                Please don't scroll past this. The Internet Archive (which runs the Wayback Machine) is a nonprofit, powered by donations averaging about $27. Join the one in a thousand users that support us financially—if our resources are useful to you, please pitch in.              </div>
+
+                          </div>
+            <div id="donate-body-background-layer2" class="supplemental-background-layer"></div>
+            <div id="donate-body-background-layer1" class="supplemental-background-layer"></div>
+          </div>
+        </div>
+
+        <div class="donate-form js-donate-form" itemscope itemtype="http://schema.org/DonateAction">
+          <meta itemprop="recipient" content="Internet Archive">
+          <meta itemprop="actionStatus" content="PotentialActionStatus">
+          <div class="white-box">
+                                <div class="donation-form-container">
+        <donation-form-edit-donation amountOptions="[5, 27, 50, 100]" amountTitleDisplayMode="slot" customAmountMode="hide" customFeesCheckboxMode="hide" frequencySelectionMode="hide" stepNumberMode="hidenumbers" class="minimal-widget focus-on-child-only" style="--paymentButtonSelectedFontColor: #fff;--paymentButtonSelectedColor: #23765D;--paymentButtonColor: #23765D;--paymentButtonFontColor: #fff;--paymentButtonBorderColor: #fff;">
+          <div slot="edit-donation-amount-title">
+            <p style="font-size: 1.8rem; line-height: calc(1.2rem * 2); margin: 0;"><b>Can You Chip In?</b> (USD)</p>
+          </div>
+        </donation-form-edit-donation>
+      </div>
+                          </div> <!--/.white-box -->
+          <div class="donate-form-chevrons" style></div>
+        </div> <!-- .donate-form -->
+      </div>
+    </header>
+        </body>
+  </html>
